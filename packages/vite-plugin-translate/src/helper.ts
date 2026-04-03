c784619ff54b5380f7e7c30cac237f648881e706
Leo Yang <qa894178522@qq.com>
2026-04-03 09:19:15 +0800

    feat: add helper function

--- packages/vite-plugin-translate/src/helper.ts
+++ packages/vite-plugin-translate/src/helper.ts
@@ -1,1 +1,30 @@
-export {}
+/**
+ * @description: 获取全局translate对象
+ * @link https://github.com/xnx3/translate
+ */
+export function getTranslate(): Record<string, any> | undefined {
+  if (!(window as any).translate) {
+    return
+  }
+
+  return (window as any).translate
+}
+
+export function executeTranslate(): void {
+  return getTranslate()?.execute()
+}
+
+export function initializeTranslate(listener?: boolean): void {
+  if (!getTranslate()) {
+    return
+  }
+
+  // 不出现的select的选择语言
+  getTranslate()!.selectLanguageTag.show = false
+  // 设置机器翻译服务通道
+  getTranslate()!.service.use('client.edge')
+  if (listener) {
+    // 开启html页面变化的监控
+    getTranslate()!.listener.start()
+  }
+}
